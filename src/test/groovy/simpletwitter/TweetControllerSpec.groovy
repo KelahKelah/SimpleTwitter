fc1be22f605701bced689471007b5d8fd0484407
KelahKelah <kelechiekebor@gmail.com>
2019-08-29 12:08:33 +0100

    Tweet Controller, Tweet Services

---
+++ src/test/groovy/simpletwitter/TweetControllerSpec.groovy
@@ -0,0 +1,18 @@
+package simpletwitter
+
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+class TweetControllerSpec extends Specification implements ControllerUnitTest<TweetController> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
